286c03a26377e475cafdc0295c33182e2ce18f0b
maratkhanovna <maratkhanovna@gmail.com>
2022-08-27 10:38:46 -0400

    three lines of additional code

--- src/test/java/class01/GitBasics.java
+++ src/test/java/class01/GitBasics.java
@@ -5,5 +5,7 @@ public class GitBasics {
         //write down a code that checks the temperature from a website and then
         //prints out that is it a beautiful day or not
         System.out.println("today is not a beautiful day");
+        System.out.println("today is not a beautiful day");
+        System.out.println("today is not a beautiful day");
     }
 }
